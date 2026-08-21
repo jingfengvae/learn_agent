c726ed8f497a4e24763e50b47ca0d0045ae8f6f9
李威 <liwei1@ushareit.com>
2026-08-21 15:15:01 +0800

    update connect_mcp_test.py

--- connect_mcp_test.py
+++ connect_mcp_test.py
@@ -9,13 +9,9 @@ result = mcp_tool.run({
     "tool_name": "add",
     "arguments":{"a": 10, "b":10}
 })
-print (f"MCP工具计算结果: result = {result}")
+print (f"MCP工具计算结果: {result}")
 
-# 2、创建GitHub MCP工具
-github_tool = MCPTool(
-    server_command = ["python", "mcp_server.py"]
-)
 # 列出github_tool可用工具
 print ("可用工具：")
-result = github_tool.run({"action": "list_tools"})
+result = mcp_tool.run({"action": "list_tools"})
 print (result)
